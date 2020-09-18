aae9fdb79551f3a4567599590c9d110e093785c0
laetitia123 <uwizelaetitia@gmail.com>
2020-09-18 12:08:06 +0200

    home and aboutus changed

--- js/javascript.js
+++ js/javascript.js
@@ -125,5 +125,9 @@ $(function() {
           });
          
       });
+      $('ul li a').click(function(){
+        $('li a').removeClass("active");
+        $(this).addClass("active");
     });
-
+    });
+   
